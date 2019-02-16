dba126ae846af04a8c8e6a002ea5e1aa66a5dd52
gquiniou <gquiniou@gmail.com>
2019-02-16 22:58:12 +0100

    GUI to edit parameters

--- main.cpp
+++ main.cpp
@@ -1,88 +1,111 @@
-    /*
-     * File:   main.cpp
-     * Author: greg
-     *
-     * Created on 4 mars 2012, 09:14
-     */
-
-    #include "Animated.hpp"
-    #include "ResourceManager.h"
+#include "imgui.h"
+#include "imgui-SFML.h"
+#include "Animated.hpp"
+#include "ResourceManager.h"
 #include <cmath>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
 
-    /*
-     *
-     */
-    int main() {
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "");
+    window.setVerticalSyncEnabled(true);
+    ImGui::SFML::Init(window);
 
-      sf::RenderWindow App(sf::VideoMode(800, 600, 32), "SFML Sandbox");
-      App.setVerticalSyncEnabled(true);
+    //window.resetGLStates(); // call it if you only draw ImGui. Otherwise not needed.
+    sf::Clock deltaClock;
 
-      sf::Sprite s;
-      s.setTexture(*game::ResourceManager::getTexture("breakout_sprites.png"));
+    int nb = 10;
+    int oldnb = nb;
+    float factor1 = 4.0;
+    float factor2 = 5.0;
+    float speed = 0.2;
+    float spacing = 1.0;
+    bool paused = false;
 
-      //Creating the sprites
-      std::vector<Animated *> sprites;
-      for (int i = 0; i < 160; i++) {
-        Animated * an = new Animated(s);
-        for (int j = 1; j < 16 ; j++) {
-          sf::IntRect rec(j*24, 376, 24, 24);
-          an->addFrame("myanim", rec, 60);
+    sf::Sprite s;
+    s.setTexture(*game::ResourceManager::getTexture("breakout_sprites.png"));
+    //Creating the sprites
+    std::vector < Animated * > sprites;
+    for (int i = 0; i < nb; i++) {
+        Animated *an = new Animated(s);
+        for (int j = 1; j < 16; j++) {
+            sf::IntRect rec(j * 24, 376, 24, 24);
+            an->addFrame("myanim", rec, 60);
         }
-        an->playanimation("myanim",i);
+        an->playanimation("myanim", i);
         sprites.push_back(an);
-      }
-
-      Animated a(s);
-      a.getDrawable().setPosition(90,90);
-      Animated a2(s);
-      a2.getDrawable().setPosition(90,120);
-      for (int i = 0; i < 16; i++) {
-        sf::IntRect rec(i*24, 376, 24, 24);
-        a.addFrame("myanim", rec, 1);
-        a2.addFrame("myanim", rec, 150);
-      }
-      a.playanimation("myanim");
-      a2.playanimation("myanim");
-
-      double i = 0;
-
-      while (App.isOpen()) {
-
-        //processing events
-        sf::Event Event;
-        while (App.pollEvent(Event)) {
-          if ((Event.type == sf::Event::Closed) || ((Event.type == sf::Event::KeyPressed) && (Event.key.code == sf::Keyboard::Escape))) {
-        return 0;
-          }
-        }
-        //Updating animation  and sprite positions
-
-        i+=0.2;
-        double j=0;
-
-        for (auto sprite : sprites) {
-          j+=1;
-          sprite->getDrawable().setPosition(385+ 380* sin(4*(i+j) * M_PI / 180), 285+280* cos(5*(i+j) * M_PI / 180));
-          sprite->update();
-        }
-        a.update();
-        a2.update();
-
-        //Drawing
-        App.clear();
-        for (auto sprite : sprites) {
-          App.draw(sprite->getDrawable());
-        }
-        App.draw(a.getDrawable());
-        App.draw(a2.getDrawable());
-
-        App.display();
-      }
-
-      for (auto sprite : sprites) {
-        delete sprite;
-      }
-      App.close();
-      return 0;
     }
+    double i = 0;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
 
+        ImGui::SFML::Update(window, deltaClock.restart());
+        ImGui::Begin("Parameters"); // begin window
+        if (ImGui::SliderInt("# of Objects", &nb, 1, 1000, "%d") || oldnb != nb) {
+            if (oldnb < nb) {
+                for (int k = 0; k < (nb - oldnb); k++) {
+                    Animated *an = new Animated(s);
+                    for (int l = 1; l < 16; l++) {
+                        sf::IntRect rec(l * 24, 376, 24, 24);
+                        an->addFrame("myanim", rec, 60);
+                    }
+                    an->playanimation("myanim", k);
+                    sprites.push_back(an);
+                }
+            }
+            if (nb < oldnb) {
+                for (int k = 0; k < (oldnb - nb); k++) {
+                    auto tmp = sprites.back();
+                    sprites.pop_back();
+                    delete tmp;
+                }
+            }
+            oldnb = nb;
+        }
+        ImGui::SliderFloat("Horizontal factor", &factor1, 0.1, 10.0, "%.1f");
+        ImGui::SliderFloat("Vertical factor", &factor2, 0.1, 10.0, "%.1f");
+        ImGui::SliderFloat("Speed", &speed, 0.05, 5.0, "%.1f");
+        ImGui::SliderFloat("Spacing", &spacing, 0.05, 5.0, "%.1f");
+        ImGui::Checkbox("Paused", &paused);
+        if (ImGui::Button("Reset")) {
+            nb = 10;
+            factor1 = 4.0;
+            factor2 = 5.0;
+            speed = 0.2;
+            spacing = 1.0;
+        }
+        if (ImGui::Button("Quit")) {
+            window.close();
+        }
+        ImGui::End(); // end window
+
+        double j = 0;
+        if (!paused) {
+            i += speed;
+        }
+        for (auto sprite : sprites) {
+            j += spacing;
+            sprite->getDrawable().setPosition(385 + 380 * sin(factor1 * (i + j) * M_PI / 180),
+                                              285 + 280 * cos(factor2 * (i + j) * M_PI / 180));
+            sprite->update();
+        }
+
+        window.clear(sf::Color::Black);
+        for (auto sprite : sprites) {
+            window.draw(sprite->getDrawable());
+        }
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+    ImGui::SFML::Shutdown();
+    for (auto sprite : sprites) {
+        delete sprite;
+    }
+}
